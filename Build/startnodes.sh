65cdc5f512ba213b88f726a459d55e1a6211ddad
Fakhri Ghribi <fakhrighribi3@gmail.com>
2022-02-01 18:01:10 +0100

    deploy ospf and configure interfaces

---
+++ Build/startnodes.sh
@@ -0,0 +1,4 @@
+pip3 install pytest
+pip3 install paramiko
+python3 Build/evesetup.py --topo CCNP/multicast_routing.unl -u
+sleep 10
---
+++ Test/testscript.sh
@@ -0,0 +1,5 @@
+ansible-playbook deploy_ospf.yml --limit multicat --verbose
+#python3 -m pytest Test/testmulticast.py --disable-warnings -s --verbose
+
+#ansible-playbook main-play-multicast.yml --limit multicat --verbose
+#python3 -m pytest Test/testmulticast.py --disable-warnings -s --verbose
